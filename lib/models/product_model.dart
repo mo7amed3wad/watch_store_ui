898bf5a0ab8c7e50a6cb7c5c3e6b46fbc089c5bc
Mohamed Awad <mo7amed3wad19@gmail.com>
2023-06-22 19:53:45 +0200

    create product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: empty_constructor_bodies
+
+class ProductModel {
+  String? image, description, brand, name, category;
+  double? price;
+
+  ProductModel(
+      {this.image,
+      this.description,
+      this.brand,
+      this.name,
+      this.category,
+      this.price});
+}
